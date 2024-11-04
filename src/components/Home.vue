fde61f6ffadf8717dbbb64c9d4c8dfd61f2d79d2
Bonggu Kang <rkdbg11752@naver.com>
2024-11-04 16:54:24 +0900

    [Home] 무비 리스트 모듈화

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <script>
-import MovieItem from "@/components/MovieItem.vue";
+import MovieList from "@/components/MovieList.vue";
 
-const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+const url = 'https://api.themoviedb.org/3/movie/';
 const options = {
   method: 'GET',
   headers: {
@@ -12,102 +12,37 @@ const options = {
 
 export default {
   name: "HomePage",
-  components: {MovieItem},
+  components: {MovieList},
   data() {
     return {
-      movieItems: []
+      MovieItemsMap: {},
     }
   },
   mounted() {
-    fetch(url, options)
-        .then(res => res.json())
-        .then(json => {
-          // 데이터를 성공적으로 가져온 후 title에 할당
-          this.movieItems = json['results'];
-        })
-        .catch(err => console.error(err));
+    this.fetchMovies('now_playing');
+    this.fetchMovies('top_rated');
+    this.fetchMovies('upcoming');
+
+    console.log(this.MovieItemsMap);
   },
   methods: {
-    nextSlide() {
-      const slider = this.$refs.slider;
-      slider.scrollBy({
-        top: 0,
-        left: slider.clientWidth,
-        behavior: 'smooth'
-      });
+    fetchMovies(listType) {
+      const language = "en-US";
+      const page = 1;
+
+      fetch(`${url}${listType}?language=${language}&page=${page}`, options)
+          .then(res => res.json())
+          .then(json => {
+            this.MovieItemsMap[listType] = json['results'];
+          })
+          .catch(err => console.error(err));
     },
-    prevSlide() {
-      const slider = this.$refs.slider;
-      slider.scrollBy({
-        top: 0,
-        left: -slider.clientWidth,
-        behavior: 'smooth'
-      });
-    }
   }
 }
 </script>
 
 <template>
-  <div>
-    <h1>Now Playing</h1>
-    <div class="slider">
-      <button @click="prevSlide" class="arrow left-arrow">❮</button>
-      <div class="movie-grid" ref="slider">
-        <MovieItem
-            v-for="item in movieItems"
-            :key="item['id']"
-            :title="item['title']"
-            :posterPath="item['poster_path']"
-            :overview="item['overview']"
-            :voteAverage="item['vote_average']"
-        />
-      </div>
-      <button @click="nextSlide" class="arrow right-arrow">❯</button>
-    </div>
-  </div>
+  <MovieList :list-name="'Now Playing'" :movie-items="MovieItemsMap['now_playing']"/>
+  <MovieList :list-name="'Top Rated'" :movie-items="MovieItemsMap['top_rated']"/>
+  <MovieList :list-name="'Upcoming'" :movie-items="MovieItemsMap['upcoming']"/>
 </template>
-
-<style scoped>
-.slider {
-  display: flex;
-  align-items: center;
-  position: relative;
-  overflow: hidden; /* 슬라이더 밖으로 넘치는 요소 숨김 */
-}
-.movie-grid {
-  display: flex;
-  overflow-x: auto; /* 수평 스크롤 가능 */
-  scroll-behavior: smooth; /* 스크롤 애니메이션 효과 */
-  width: 100%;
-  padding: 10px 0; /* 패딩 추가 */
-  gap: 16px;
-}
-.movie-grid::-webkit-scrollbar {
-  display: none; /* Chrome/Safari용 */
-}
-.arrow {
-  background-color: transparent;
-  border: none;
-  color: grey;
-  cursor: pointer;
-  font-size: 24px;
-  padding: 10px;
-  position: absolute;
-  z-index: 1;
-  top: 50%;
-  transform: translateY(-50%);
-}
-/* hover 효과 */
-.arrow:hover {
-  color: black;
-}
-.left-arrow {
-  left: 10px;
-}
-
-.right-arrow {
-  right: 10px;
-}
-
-</style>
---
+++ src/components/MovieList.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <h1>{{listName}}</h1>
+    <div class="slider">
+      <button @click="prevSlide" class="arrow left-arrow">❮</button>
+      <div class="movie-grid" ref="slider">
+        <MovieItem
+            v-for="item in movieItems"
+            :key="item['id']"
+            :title="item['title']"
+            :posterPath="item['poster_path']"
+            :overview="item['overview']"
+            :voteAverage="item['vote_average']"
+        />
+      </div>
+      <button @click="nextSlide" class="arrow right-arrow">❯</button>
+    </div>
+  </div>
+</template>
+<script>
+import MovieItem from "@/components/MovieItem.vue"
+
+export default {
+  name: 'MovieList',
+  components: {MovieItem},
+  props: {
+    listName: String,
+    movieItems: Array,
+  },
+  mounted() {
+    console.log(this.movieItems);
+  },
+  methods: {
+    nextSlide() {
+      const slider = this.$refs.slider;
+      slider.scrollBy({
+        top: 0,
+        left: slider.clientWidth,
+        behavior: 'smooth'
+      });
+    },
+    prevSlide() {
+      const slider = this.$refs.slider;
+      slider.scrollBy({
+        top: 0,
+        left: -slider.clientWidth,
+        behavior: 'smooth'
+      });
+    }
+  }
+}
+</script>
+<style scoped>
+.slider {
+  display: flex;
+  align-items: center;
+  position: relative;
+  overflow: hidden; /* 슬라이더 밖으로 넘치는 요소 숨김 */
+}
+
+.movie-grid {
+  display: flex;
+  overflow-x: auto; /* 수평 스크롤 가능 */
+  scroll-behavior: smooth; /* 스크롤 애니메이션 효과 */
+  width: 100%;
+  padding: 10px 0; /* 패딩 추가 */
+  gap: 16px;
+}
+
+.movie-grid::-webkit-scrollbar {
+  display: none; /* Chrome/Safari용 */
+}
+
+.arrow {
+  background-color: transparent;
+  border: none;
+  color: grey;
+  cursor: pointer;
+  font-size: 24px;
+  padding: 10px;
+  position: absolute;
+  z-index: 1;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+/* hover 효과 */
+.arrow:hover {
+  color: black;
+}
+
+.left-arrow {
+  left: 10px;
+}
+
+.right-arrow {
+  right: 10px;
+}
+
+</style>

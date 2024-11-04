c82dd345f8f3325a12cb8547ad006aa3739714c6
Bonggu Kang <rkdbg11752@naver.com>
2024-11-04 15:38:06 +0900

    [Popular] Top 버튼 추가

--- src/components/Popular.vue
+++ src/components/Popular.vue
@@ -50,6 +50,9 @@ export default {
         this.fetchMovies(--this.currentPage);
       }
     },
+    goTop() {
+      window.scrollTo(0, 0);
+    },
     setViewOption(option) {
       this.viewOption = option; // 선택된 옵션 설정
       this.currentPage = 1; // 페이지를 1로 초기화
@@ -120,6 +123,9 @@ export default {
         />
       </div>
     </div>
+    <div class="go-top-button">
+      <button @click="goTop">Top</button>
+    </div>
   </div>
 </template>
 
@@ -147,5 +153,13 @@ export default {
 }
 .pagination button {
   margin: 0 10px;
+}
+.go-top-button {
+  position: fixed; /* 화면에 고정 */
+  bottom: 20px; /* 하단에서 20px */
+  right: 20px; /* 우측에서 20px */
+  padding: 10px 15px; /* 패딩 */
+  border-radius: 5px; /* 모서리 둥글게 */
+  z-index: 1000; /* 다른 요소 위에 표시 */
 }
 </style>

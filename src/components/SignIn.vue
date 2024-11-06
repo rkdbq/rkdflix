897e993ac310f86cc236402bc3293b41f0873e33
Bonggu Kang <rkdbg11752@naver.com>
2024-11-06 11:27:08 +0900

    [SignIn] 회원가입 페이지 구현

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,20 +1,91 @@
 <script>
 export default {
   name: "SignIn",
+  data() {
+    return {
+      isLogin: true,
+      buttonLabel: ["회원가입", "로그인"],
+
+      userId: '',
+      userPassword: '',
+
+      userIdRegister: '',
+      userPasswordRegister: '',
+      userPasswordConfirmRegister: '',
+      conditionAgreementRegister: false,
+    }
+  },
   methods: {
     LogIn() {
+      console.log("ID:", this.userId);
+      console.log("Password:", this.userPassword);
+
       this.$router.push('/')
     },
+    Toggle() {
+      this.isLogin = !this.isLogin;
+    },
+    Register() {
+      console.log("Register:", this.userIdRegister);
+      console.log("Register:", this.userPasswordRegister);
+      console.log("Register:", this.userPasswordConfirmRegister);
+      console.log("Register:", this.conditionAgreementRegister);
+    }
   },
 }
 </script>
 
 <template>
+  <div v-if="isLogin" class="sign-in-container">
+    <div>
+      <label for="id">아이디:</label>
+      <input type="text" id="id" v-model="userId" />
+    </div>
+    <div>
+      <label for="password">비밀번호:</label>
+      <input type="password" id="password" v-model="userPassword" />
+    </div>
+  </div>
+
+  <div v-if="!isLogin" class="register-container">
+    <div>
+      <label for="id">아이디:</label>
+      <input type="text" id="id" v-model="userIdRegister" />
+    </div>
+    <div>
+      <label for="password">비밀번호:</label>
+      <input type="password" id="password" v-model="userPasswordRegister" />
+    </div>
+    <div>
+      <label for="password">비밀번호 확인:</label>
+      <input type="password" id="password-confirm" v-model="userPasswordConfirmRegister" />
+    </div>
+    <div>
+      <label for="password">약관 동의: </label>
+      <input type="checkbox" id="condition-agreement" v-model="conditionAgreementRegister" />
+    </div>
+  </div>
+
   <div>
-    <button @click="LogIn">Log In</button>
+    <button v-if="isLogin" @click="LogIn">로그인</button>
+    <button v-if="!isLogin" @click="Register">회원가입</button>
+  </div>
+  <div>
+    <button @click="Toggle">{{ this.buttonLabel[Number(!this.isLogin)] }}하기</button>
   </div>
 </template>
 
 <style scoped>
+div {
+  margin-bottom: 10px;
+}
 
+label {
+  margin-right: 8px;
+}
+
+input {
+  padding: 5px;
+  font-size: 14px;
+}
 </style>

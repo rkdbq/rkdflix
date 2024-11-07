41d83fb3d71d669030f8bd7acdc3611670c40083
Bonggu Kang <rkdbg11752@naver.com>
2024-11-07 18:22:16 +0900

    CompositionAPI로 리팩터링

--- src/components/movie/MovieScrollView.vue
+++ src/components/movie/MovieScrollView.vue
@@ -1,32 +1,41 @@
 <template>
-  <MovieGrid :movie-items="movieItems" :show-info="showInfo"/>
+  <MovieGrid :movie-items="movieItems" :show-info="showInfo" />
   <div class="go-top-button">
     <button @click="goTop">Top</button>
   </div>
 </template>
-<script>
-import MovieGrid from "@/components/movie/MovieGrid.vue"
 
-export default {
+<script>
+import { defineComponent } from 'vue';
+import MovieGrid from "@/components/movie/MovieGrid.vue";
+
+export default defineComponent({
   name: 'MovieScrollView',
-  components: {MovieGrid},
+  components: { MovieGrid },
   props: {
-    goTop: {},
-    movieItems: [],
+    goTop: {
+      type: Function,
+      required: true
+    },
+    movieItems: {
+      type: Array,
+      required: true
+    },
     showInfo: {
       type: Boolean,
-      default: false,
+      default: false
     }
   }
-}
+});
 </script>
+
 <style scoped>
 .go-top-button {
-  position: fixed; /* 화면에 고정 */
-  bottom: 20px; /* 하단에서 20px */
-  right: 20px; /* 우측에서 20px */
-  padding: 10px 15px; /* 패딩 */
-  border-radius: 5px; /* 모서리 둥글게 */
-  z-index: 1000; /* 다른 요소 위에 표시 */
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  z-index: 1000;
 }
 </style>
--- src/components/movie/MovieTableView.vue
+++ src/components/movie/MovieTableView.vue
@@ -1,25 +1,40 @@
 <template>
-  <MovieGrid :movie-items="movieItems"/>
+  <MovieGrid :movie-items="movieItems" />
   <div class="pagination">
     <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
     <span>Page {{ currentPage }}</span>
     <button @click="nextPage" :disabled="currentPage === 10">Next</button>
   </div>
 </template>
-<script>
-import MovieGrid from "@/components/movie/MovieGrid.vue"
 
-export default {
+<script>
+import { defineComponent } from 'vue';
+import MovieGrid from "@/components/movie/MovieGrid.vue";
+
+export default defineComponent({
   name: 'MovieTableView',
-  components: {MovieGrid},
+  components: { MovieGrid },
   props: {
-    currentPage: {},
-    movieItems: {},
-    nextPage: {},
-    prevPage: {}
-  }
-}
+    currentPage: {
+      type: Number,
+      required: true
+    },
+    movieItems: {
+      type: Array,
+      required: true
+    },
+    nextPage: {
+      type: Function,
+      required: true
+    },
+    prevPage: {
+      type: Function,
+      required: true
+    }
+  },
+});
 </script>
+
 <style scoped>
 .pagination {
   display: flex;
